794a73a33311f0501c7fe1f5ae1f103f40a24e16
Alexandra Mesina <alexandra.mesina@yahoo.com>
2023-07-02 15:20:13 +0300

    Modified

--- src/screens/Budget.js
+++ src/screens/Budget.js
@@ -1,4 +1,5 @@
 /* eslint-disable prettier/prettier */
+/* eslint-disable prettier/prettier */
 import {
   StyleSheet,
   Text,
@@ -10,97 +11,109 @@ import {
 } from 'react-native';
 import React, {useState, useEffect} from 'react';
 import PieChart from 'react-native-pie-chart';
-import {fetchTransactions} from './FirebaseFunctions';
+import DropDownPicker from 'react-native-dropdown-picker';
+import {fetchTransactionsByDate} from './FirebaseFunctions';
 
 export default function Budget({navigation}) {
-  useEffect(() => {
-    getTransactions();
-  }, []);
-
   const [transactions, setTransactions] = useState([]);
-  const [mancare, setMancare] = useState();
-  const [chirie, setChirie] = useState();
-  const [facturi, setFacturi] = useState();
-  const [abonamente, setAbonamente] = useState();
-  const [altele, setAltele] = useState();
+  const [expensesData, setExpensesData] = useState([]);
   const [refreshing, setRefreshing] = useState(false);
+  const [selectedMonth, setSelectedMonth] = useState(null);
+  const [open, setOpen] = useState(false);
+  const monthOptions = [
+    {label: 'Luna curenta', value: 0},
+    {label: 'Cu 1 luna in urma', value: 1},
+    {label: 'Cu 2 luni in urma', value: 2},
+    // Add more options if needed
+  ];
+
+  useEffect(() => {
+    if (selectedMonth !== null) {
+      getTransactions();
+    }
+  }, [selectedMonth]);
+
   const onRefresh = () => {
     setRefreshing(true);
     getTransactions();
     setRefreshing(false);
   };
+
   const getTransactions = async () => {
     try {
-      const fetchedTransactions = await fetchTransactions('tranzactii');
+      const currentDate = new Date();
+      const selectedDate = new Date();
+      selectedDate.setMonth(currentDate.getMonth() - selectedMonth);
+      selectedDate.setDate(1);
+
+      const lastDayOfMonth = new Date(
+        currentDate.getFullYear(),
+        currentDate.getMonth() - selectedMonth + 1,
+        0,
+      ).getDate();
+
+      const fetchedTransactions = await fetchTransactionsByDate(
+        'tranzactii',
+        selectedDate,
+        new Date(
+          selectedDate.getFullYear(),
+          selectedDate.getMonth(),
+          lastDayOfMonth,
+        ),
+      );
+
       setTransactions(fetchedTransactions);
 
-      let mancareTotal = 0;
-      let chirieTotal = 0;
-      let facturiTotal = 0;
-      let abonamenteTotal = 0;
-      let alteleTotal = 0;
-      console.log(fetchedTransactions);
-      fetchedTransactions.map(transaction => {
-        if (transaction.Desc === 'Mancare' && transaction.este_plata === 'da') {
-          mancareTotal += transaction.Suma;
-        } else if (
-          transaction.Desc === 'Chirie' &&
-          transaction.este_plata === 'da'
+      const expensesByCategory = {};
+
+      fetchedTransactions.forEach(transaction => {
+        if (
+          transaction.este_plata === 'da' &&
+          typeof transaction.Desc === 'string' &&
+          typeof transaction.Suma === 'number'
         ) {
-          chirieTotal += transaction.Suma;
-        } else if (
-          transaction.Desc === 'Facturi' &&
-          transaction.este_plata === 'da'
-        ) {
-          facturiTotal += transaction.Suma;
-        } else if (
-          transaction.Desc === 'Abonamente' &&
-          transaction.este_plata === 'da'
-        ) {
-          abonamenteTotal += transaction.Suma;
-        } else if (
-          transaction.Desc === 'Altele' &&
-          transaction.este_plata === 'da'
-        ) {
-          alteleTotal += transaction.Suma;
+          if (!expensesByCategory[transaction.Desc]) {
+            expensesByCategory[transaction.Desc] = 0;
+          }
+          expensesByCategory[transaction.Desc] += transaction.Suma;
         }
       });
-      console.log(mancareTotal);
-      console.log(chirieTotal);
 
-      setMancare(mancareTotal);
-      setChirie(chirieTotal);
-      setFacturi(facturiTotal);
-      setAbonamente(abonamenteTotal);
-      setAltele(alteleTotal);
+      const expensesData = Object.entries(expensesByCategory).map(
+        ([category, amount]) => ({
+          category,
+          amount,
+          color: getRandomColor(),
+        }),
+      );
+
+      setExpensesData(expensesData);
     } catch (error) {
       console.error('Error fetching transactions: ', error);
+      // Handle the error here
+      setExpensesData([]);
     }
   };
 
-  const expensesData = [
-    {category: 'Mancare', amount: mancare, color: '#CCE3DE'},
-    {category: 'Chirie', amount: chirie, color: '#006d77'},
-    {category: 'Facturi', amount: facturi, color: '#A4C3B2'},
-    {category: 'Abonamente', amount: abonamente, color: '#e29578'},
-    {category: 'Altele', amount: altele, color: '#ffddd2'},
-  ];
-
-  if (!expensesData || expensesData.length === 0) {
-    // Handle the case when expensesData is undefined or empty
-    return (
-      <View style={styles.container}>
-        <Text>No expenses data available</Text>
-      </View>
-    );
-  }
-  const totalAmount = expensesData.reduce(
-    (sum, expense) => sum + expense.amount,
-    0,
-  );
+  const getRandomColor = () => {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  };
 
   const renderItem = ({item}) => {
-    const percentage = ((item.amount / totalAmount) * 100).toFixed(2);
+    const totalAmount = expensesData.reduce(
+      (sum, expense) => sum + expense.amount,
+      0,
+    );
+
+    let percentage = 0;
+    if (totalAmount !== 0) {
+      percentage = ((item.amount / totalAmount) * 100).toFixed(2);
+    }
     return (
       <View style={styles.legendItem}>
         <View style={[styles.legendColor, {backgroundColor: item.color}]} />
@@ -111,41 +124,78 @@ export default function Budget({navigation}) {
   };
 
   return (
-    <ScrollView
-      contentContainerStyle={styles.scrollViewContent}
-      refreshControl={
-        <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
-      }>
-      <View style={styles.headerBuget}>
-        <Text style={styles.textHeader}>
-          Aici poti vedea raportul cheltuielilor tale
-        </Text>
-      </View>
-      <View style={styles.container}>
-        <PieChart
-          widthAndHeight={250}
-          series={expensesData.map(expense => expense.amount)}
-          sliceColor={['#CCE3DE', '#006d77', '#A4C3B2', '#ffddd2', '#e29578']}
-          coverRadius={0.6}
-          coverFill={'#FFF'}
-          accessor="amount"
-          backgroundColor="transparent"
-          paddingLeft="15"
-          absolute
+    <View style={styles.big}>
+      <ScrollView
+        contentContainerStyle={styles.scrollViewContent}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }>
+        <View style={styles.headerBuget}>
+          <Text style={styles.textHeader}>
+            Aici poti vedea raportul cheltuielilor tale
+          </Text>
+        </View>
+        <View style={styles.container}>
+          {expensesData.length > 0 ? (
+            <>
+              <PieChart
+                widthAndHeight={250}
+                series={expensesData.map(expense => expense.amount)}
+                sliceColor={expensesData.map(expense => expense.color)}
+                coverRadius={0.6}
+                coverFill={'#FFF'}
+                accessor="amount"
+                backgroundColor="transparent"
+                paddingLeft="15"
+                absolute
+              />
+              <FlatList
+                data={expensesData}
+                renderItem={renderItem}
+                keyExtractor={item => item.category}
+                horizontal={true}
+                contentContainerStyle={styles.legendContainer}
+              />
+            </>
+          ) : (
+            <Text style={styles.faraDate}>
+              Nu exista date pentru perioada selectata
+            </Text>
+          )}
+        </View>
+      </ScrollView>
+      <View style={styles.dropdownContainer}>
+        <DropDownPicker
+          items={monthOptions}
+          open={open}
+          setOpen={setOpen}
+          value={selectedMonth}
+          containerStyle={{height: 40}}
+          style={{backgroundColor: '#fafafa'}}
+          itemStyle={{justifyContent: 'flex-start'}}
+          dropDownStyle={{backgroundColor: '#fafafa'}}
+          setValue={setSelectedMonth}
+          onChangeItem={item => setSelectedMonth(item.value)}
+          placeholder="Selecteaza luna"
+          dropDownMaxHeight={150}
+          zIndex={9999}
         />
-        <FlatList
-          data={expensesData}
-          renderItem={renderItem}
-          keyExtractor={item => item.category}
-          horizontal={true}
-          contentContainerStyle={styles.legendContainer}
-        />
       </View>
-    </ScrollView>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
+  big: {
+    height: '80%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  faraDate: {
+    fontSize: 20,
+    textAlign:'center',
+    color:'red',
+  },
   headerBuget: {
     flex: 1,
     alignItems: 'center',
@@ -158,11 +208,18 @@ const styles = StyleSheet.create({
   },
   scrollViewContent: {
     flexGrow: 1,
+    justifyContent: 'center',
   },
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  dropdownContainer: {
+    width: '80%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 10,
   },
   legendContainer: {
     marginTop: 10,
--- src/screens/Reports.js
+++ src/screens/Reports.js
@@ -39,7 +39,6 @@ const Reports = () => {
 
   const handleAccountChange = value => {
     setSelectedAccount(value);
-
   };
 
   const handleExcelExport = async () => {
